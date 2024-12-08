becf3dabd66c5ac65b8ff0f02aa991951f91f036
linjunjie <3185058251@qq.com>
2024-12-08 22:37:05 +0800

    不用footer

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,13 +3,12 @@
     <Header></Header>
     <Snow></Snow>
     <router-view style="min-height: 800px"></router-view>
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+// import Footer from '../components/Footer.vue'
 import Snow from '../components/snow.vue'
 
 export default {
@@ -18,7 +17,7 @@ export default {
     Footer,
     Snow,
   },
-  data() {
+  data () {
     return {}
   },
 }
