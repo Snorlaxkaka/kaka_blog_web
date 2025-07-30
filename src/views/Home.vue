dcd3b36ec10627b2ac7199799ca95199472eed72
linjunjie <129569154+Snorlaxkaka@users.noreply.github.com>
2025-07-30 15:58:39 +0800

    Update:footer组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
+<!--
+ * @Author: linjunjie 129569154+Snorlaxkaka@users.noreply.github.com
+ * @Date: 2025-07-30 15:16:31
+ * @LastEditors: linjunjie 129569154+Snorlaxkaka@users.noreply.github.com
+ * @LastEditTime: 2025-07-30 15:52:49
+ * @FilePath: \kaka_blog_web\src\views\Home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="mb-9">
     <div class="mb-7">
       <div class="bgImg d-flex jc-center" id="header">
         <div class="text-white d-flex jc-center ai-center">
-          <span class="shouye-text">kaka的个人博客网站</span>
+          <span class="shouye-text">chengfen的个人前端博客</span>
         </div>
         <div class="arrow-down">
           <i class="iconfont icon-down" @click="downPage"></i>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,24 @@
+<!--
+ * @Author: linjunjie 129569154+Snorlaxkaka@users.noreply.github.com
+ * @Date: 2025-07-30 15:16:31
+ * @LastEditors: linjunjie 129569154+Snorlaxkaka@users.noreply.github.com
+ * @LastEditTime: 2025-07-30 15:51:02
+ * @FilePath: \kaka_blog_web\src\views\Main.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="main">
     <Header></Header>
     <Snow></Snow>
     <router-view style="min-height: 800px"></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue'
-// import Footer from '../components/Footer.vue'
-import Snow from '../components/snow.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import Snow from '@/components/Snow.vue'
 
 export default {
   components: {
