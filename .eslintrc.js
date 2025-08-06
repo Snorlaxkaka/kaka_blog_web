a936c46f6a7af76c890486f7cbc129768ac862f0
linjunjie <129569154+Snorlaxkaka@users.noreply.github.com>
2025-08-06 11:59:44 +0800

    update:修改eslint配置规范化

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: linjunjie 129569154+Snorlaxkaka@users.noreply.github.com
+ * @Date: 2025-08-06 10:31:56
+ * @LastEditors: linjunjie 129569154+Snorlaxkaka@users.noreply.github.com
+ * @LastEditTime: 2025-08-06 10:32:06
+ * @FilePath: \kaka_blog_web\.eslintrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: ['eslint:recommended', 'plugin:vue/essential'],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        // 可以在这里自定义规则
+    }
+}
